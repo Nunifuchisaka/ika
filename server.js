dc46e987aacccb9881c58f08f353f8cbb745838a
Nunifuchisaka <nunifuchisaka@gmail.com>
2016-02-02 22:20:45 +0900

    ectとlodashのテンプレートの衝突どうしたらええねん...

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+var port = 3000;
 var express = require('express');
-var path = require('path');
 var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-//app.use(express.static('htdocs'));
+
+// ECT view engine setup
+/*
+var ECT = require('ect');
+var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+*/
+
+// routes setup
+/*
+var routes = require('./routes/index');
+app.use('/', routes);
+*/
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//
 io.on('connection', function(socket){
   socket.on('send_message', function(text){
     io.sockets.emit('receive_message',text);
@@ -17,7 +35,10 @@ io.on('connection', function(socket){
   });
 });
 
-var port = 3000;
+//
 http.listen(port,function(){
   console.log("Expressサーバーがポート%dで起動しました。モード:%s",port,app.settings.env)
 });
+
+
+module.exports = app;
